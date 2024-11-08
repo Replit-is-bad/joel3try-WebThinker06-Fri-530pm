fb35881982fe6e06a324ceb43e85237c602cc42f
Replit-is-bad <ylchia_hill@hotmail.com>
2024-11-08 10:43:12 +0000

    end of lesson 18

--- script/projects.js
+++ script/projects.js
@@ -1,26 +1,55 @@
-let canvas,ctx;
-let centerX ,centerY;
+let canvas, ctx;
+let centerX, centerY;
+let ballX, ballY;
+let dx, dy;
 
 
-window.onload = () => {
-    canvas =document.getElementById("drawingboard");
-    ctx = canvas.getContext("2d");
-
-    centerX = canvas.width / 2;
-    centerY =canvas.height / 2;
+let drawBall = () => {
+	ctx.beginPath();
+	ctx.arc(centerX, centerY, 0, Math.PI * 2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
 }
 
- let drawball =() => {
-    ctx.lineWidth =10;
-    ctx.strokeStyle ="rgb(255,0,0)"
-    ctx.fillStyle ="rgb(0,0,255)"
 
-    ctx.beginPath();
-    ctx.arc(centerX,centerY,25,0,2 * Math.PI );
-    ctx.fill();
-    ctx.stroke();
+let animating = () => {
 
- }
-let refreshAnimation = () => {
+	if (document.getElementById("playAnimation").checked) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		drawBall();
 
+	
+		if (ballX < 0 || ballx> canvas.width) {
+			dx = -dx;
+		}
+		
+		if (ballY < 0 || ballY >canvas.height) {
+			dy = -dy;
+		}
+
+
+		x += dx;
+		y += dy;
+        drawBall();
+
+    requestAnimationFrame(animating);
+	}
+    window.onload = () => {
+
+        canvas = document.getElementById('canvasBoard');
+        ctx = canvas.getContext('2d');
+    
+    
+        centerX =canvas.width /2;
+        centerY =center.height /2;
+    
+        ballx = centerX
+        bally =centerY
+        dx = 2;
+        dy = -2;
+    
+        drawBall();
+        animating();
+    }
+	
 }
